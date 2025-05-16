d044f5692eaa552fe210e5cc1611ece6a88464ae
Jean Bairro <jeancasulo@gmail.com>
2025-05-15 23:02:25 -0300

    Add window with purple rect example

--- main.go
+++ main.go
@@ -12,5 +12,34 @@ func main() {
 	}
 	defer sdl.Quit()
 
-	fmt.Print("Dengueboy")
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	color := sdl.Color{R: 255, G: 0, B: 255, A: 255}
+	pixel := sdl.MapRGBA(surface.Format, color.R, color.G, color.B, color.A)
+	surface.FillRect(&rect, pixel)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+			}
+		}
+
+		sdl.Delay(10)
+	}
 }
